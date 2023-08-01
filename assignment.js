279777764cccbfefc9a42b9190673bfc3da8c831
MikiArai-TMC <137766378+MikiArai-TMC@users.noreply.github.com>
2023-08-01 18:52:54 +0900

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -10,43 +10,74 @@ function findMyDate(){
 
 
 
+function selectboxChange() {
+  target = document.getElementById("output");
 
-
-
-
-function getSymbol(insertedDate) {  
-    if (insertedDate === "月曜日") {
-      return "トラ(東)"; 
-    } else if (insertedDate === "火曜日") {
-      return "ライオン(南東)"; 
-    } else if (insertedDate === "水曜日午前") {
-      return "牙のあるゾウ(南)"; 
-    } else if (insertedDate === "水曜日午後") {
-      return "牙のない像(北西)"; 
-    } else if (insertedDate === "木曜日") {
-      return "ねずみ(西)"; 
-    } else if (insertedDate === "金曜日") {
-      return "モグラ(北)"; 
-    } else if (insertedDate === "土曜日") {
-      return "龍(南西)"; 
-    } else if (insertedDate === "日曜日") {
-      return "ガルーダ(北東)"; 
-    } else {
-      return "無効な値"; 
-    }
+  selindex = document.form5.Select1.selectedIndex;
+  switch (selindex) {
+    case 1:
+      target.innerHTML = "トラ(東)<br/>";
+      break;
+    case 2:
+      target.innerHTML = "ライオン(南東)<br/>";
+      break;
+    case 3:
+      target.innerHTML = "牙のあるゾウ(南)<br/>";
+      break;
+    case 4:
+      target.innerHTML = "牙のない像(北西)<br/>";
+      break;
+    case 5:
+      target.innerHTML = "ねずみ(西)<br/>";
+      break;
+      case 6:
+      target.innerHTML = "モグラ(北)<br/>";
+      break;
+      case 7:
+      target.innerHTML = "龍(南西)<br/>";
+      break;
+      case 8:
+      target.innerHTML = "ガルーダ(北東)<br/>";
+      break;     
   }
+}
+
+
+
+
+// function getSymbol(insertedDate) {  
+//     if (insertedDate === "月曜日") {
+//       return "トラ(東)"; 
+//     } else if (insertedDate === "火曜日") {
+//       return "ライオン(南東)"; 
+//     } else if (insertedDate === "水曜日午前") {
+//       return "牙のあるゾウ(南)"; 
+//     } else if (insertedDate === "水曜日午後") {
+//       return "牙のない像(北西)"; 
+//     } else if (insertedDate === "木曜日") {
+//       return "ねずみ(西)"; 
+//     } else if (insertedDate === "金曜日") {
+//       return "モグラ(北)"; 
+//     } else if (insertedDate === "土曜日") {
+//       return "龍(南西)"; 
+//     } else if (insertedDate === "日曜日") {
+//       return "ガルーダ(北東)"; 
+//     } else {
+//       return "無効な値"; 
+//     }
+//   }
   
-  console.log(getSymbol("月曜日"));
+//   console.log(getSymbol("月曜日"));
   
-  let insertedDate = document.getElementById('date');
-  let msg = document.getElementById('msg');
-  let checkButton = document.getElementById('checkButton');
+//   let insertedDate = document.getElementById('date');
+//   let msg = document.getElementById('msg');
+//   let checkButton = document.getElementById('checkButton');
   
-  function buttonClick() {
-    msg.innerText = 'シンボルは' + getSymbol(insertedDate.value) ;
-  }
+//   function buttonClick() {
+//     msg.innerText = 'シンボルは' + getSymbol(insertedDate.value) ;
+//   }
   
-  checkButton.addEventListener('click', buttonClick);
+//   checkButton.addEventListener('click', buttonClick);
 
 
 
